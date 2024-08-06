5afc26121f4a346e8d7e64d741657a79c9ca8a56
mmgonnar <gn.mariela@gmail.com>
2024-08-06 17:42:44 -0600

    Copied header html

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header class="header">
+      <img
+        src="./images/logo_aus.svg"
+        alt="Logo Around the US"
+        class="header__logo"
+      />
+    </header>
+  );
+};
