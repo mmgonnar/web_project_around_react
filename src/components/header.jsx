d9055262e1c1fd93c2298a23f24268964764d924
mmgonnar <gn.mariela@gmail.com>
2024-08-06 19:36:21 -0600

    Added footer.jsx

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header class="header">
+      <img src="/logo_aus.svg" alt="Logo Around the US" class="header__logo" />
+    </header>
+  );
+};
+
+export default Header;
