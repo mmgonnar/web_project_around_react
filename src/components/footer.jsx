63c90c4edf4bcc3c73db5f2842f721329b20dec1
mmgonnar <gn.mariela@gmail.com>
2024-08-06 19:38:13 -0600

    Moved html info

---
+++ src/components/footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <div class="footer__text">
+        <p class="footer__copyright">
+          &#169 2023 Around The U.S. |{" "}
+          <strong>
+            <a
+              href="https://github.com/mmgonnar"
+              target="_blank"
+              class="footer__link"
+            >
+              Mariela González
+            </a>
+          </strong>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
