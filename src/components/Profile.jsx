b0d3c091d3db6cfec635248d037c1675b4492548
mmgonnar <gn.mariela@gmail.com>
2024-08-06 20:29:20 -0600

    Added html to profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () {
+  return(
+    <section class="profile">
+        <div class="profile__container">
+          <img
+          src="<%=require('./images/profile.png')%>"
+          alt="profile__avatar"
+          class="profile__avatar"
+        />
+          <button class="button button_edit-avatar"></button>
+          <div class="profile__overlay"></div>
+        </div>
+        <div class="profile__info">
+          <div class="profile__info-container">
+            <p class="profile__name">Jacques Cousteau</p>
+            <button class="button button_edit"></button>
+          </div>
+          <p class="profile__job">Explorer</p>
+        </div>
+        <button class="button button_add"></button>
+      </section>
+  )
+}
