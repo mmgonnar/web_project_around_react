eefa1437305766c8f26b32895b6e3d333f6e95bc
mmgonnar <gn.mariela@gmail.com>
2024-08-09 18:53:30 -0600

    Added name to form

---
+++ src/components/PopupWithForm.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef } from "react";
+
+const PopupWithForm = (props) => {
+  const popupRef = useRef(null);
+  const handleClose = (evt) => {
+    const popupId = popupRef.current.id;
+    props.onClose(popupId);
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (evt) => {
+      if (evt.key === "Escape") {
+        handleClose(evt);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+  return (
+    <>
+      <div
+        ref={popupRef}
+        id={props.name}
+        className={`popup popup_${props.name} ${
+          props.isOpened ? "popup_show" : ""
+        }`}
+      >
+        <div onClick={handleClose} className="popup__overlay"></div>
+        <div className="popup__content">
+          <div className="popup__container">
+            <button
+              onClick={handleClose}
+              className="button button_close"
+            ></button>
+            <span className="popup__title">{props.title}</span>
+            <form
+              className="popup__form"
+              id={`form-${props.name}`}
+              name={props.name}
+              noValidate
+            >
+              {props.children}
+              <button
+                type="submit"
+                className="button button_submit button_submit-disabled"
+                onClick={handleClose}
+              >
+                Save
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PopupWithForm;
