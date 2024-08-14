a44cf05cf8c5d77a3c37984e07ceab405e094189
mmgonnar <gn.mariela@gmail.com>
2024-08-13 19:29:01 -0600

    Fixed last updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__text">
         <p className="footer__copyright">
-          {" \u00A9"} 2023 Around The U.S. |{" "}
+          {" \u00A9"} {new Date().getFullYear()} Around The U.S. |{" "}
           <strong>
             <a
               href="https://github.com/mmgonnar"
