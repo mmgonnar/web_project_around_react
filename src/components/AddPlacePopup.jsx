fa6877ef9b59cc363f87ee6d81dd39a1b8673e1a
mmgonnar <gn.mariela@gmail.com>
2024-10-09 17:25:55 -0600

    Moved html to component

--- src/components/AddPlacePopup.jsx
+++ src/components/AddPlacePopup.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import PopupWithForm from "./PopupWithForm";
 
 const AddPlacePopup = () => {
-  return <></>;
+  return (
+    <PopupWithForm
+      title="New Place"
+      name="add"
+      isOpened={props.isOpened}
+      onClose={props.onClose}
+    >
+      <fieldset className="popup__set">
+        <div className="popup__container-input">
+          <input
+            type="text"
+            name="title"
+            id="input-title"
+            className="popup__input popup__input-title"
+            placeholder="Title"
+            minLength="2"
+            maxLength="30"
+            required
+            autoComplete="on"
+          />
+          <span className="popup__error" id="input__error-title"></span>
+        </div>
+        <div className="popup__container-input">
+          <input
+            type="url"
+            name="url"
+            id="input-image-add"
+            className="popup__input popup__input-image"
+            placeholder="Image URL"
+            minLength="6"
+            required
+            autoComplete="on"
+          />
+          <span className="popup__error" id="input__error-url"></span>
+        </div>
+      </fieldset>
+    </PopupWithForm>
+  );
 };
 
 export default AddPlacePopup;
