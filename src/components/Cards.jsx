c1ffe6bd03d09ae3ad15c00d6bf0bf4191c85489
mmgonnar <gn.mariela@gmail.com>
2024-10-10 10:04:52 -0600

    Cleaned code in Cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,13 +8,6 @@ const Cards = (props) => {
     <div className="cards" id="cards-template">
       {props.cards.map((card) => {
         const isOwn = card.owner._id === currentUser._id;
-        //console.log(isOwn);
-        /* const cardDeleteButtonClassName = `button_delete  ${
-          isOwn ? "button_delete _visible" : "button_delete _hidden"
-        }`; */
-        // const isLiked = card.likes.some((i) => i._id === currentUser._id);
-        // console.log(isLiked);
-        // const cardLikeButtonClassName = `...`;
 
         return (
           <div className="card" id={card._id} key={card._id}>
@@ -27,20 +20,16 @@ const Cards = (props) => {
               </p>
               <div className="card__likes">
                 <button
-                  //className="button button_like"
                   className={`button_like ${
-                    //isLiked ? "button_lie.liked" : ""
                     card.likes.some((like) => like._id === currentUser._id)
                       ? "button_like liked"
                       : ""
                   }`}
-                  //onClick={() => onCardLike(card)}
                   onClick={() => props.onCardLike(card)}
                 ></button>
                 <p className="card__counter">{card.likes.length}</p>
               </div>
               <button
-                //className="button_delete"
                 className={`${isOwn ? "button_delete" : ""}`}
                 onClick={() => props.onCardDelete(card._id)}
               ></button>
