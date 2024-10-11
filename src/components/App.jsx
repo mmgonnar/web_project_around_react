cf8d722d76abca64f1af39781c108a478c5b688d
mmgonnar <gn.mariela@gmail.com>
2024-10-11 09:21:44 -0600

    Deleted some more consle.log

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -53,7 +53,7 @@ function App() {
         newCard = await api.deleteLikeCard(card._id);
       } else {
         newCard = await api.likeCard(card._id);
-        console.error("Liked Card");
+        console.error("Liked Card", error);
       }
 
       setCards((state) => state.map((c) => (c._id === card._id ? newCard : c)));
